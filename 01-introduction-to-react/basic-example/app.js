85cdf47f5d4c3b0e894d1af193996e87710a45f2
eminero <minero.emer@gmail.com>
2021-05-28 19:13:58 -0600

    adding app.js

---
+++ 01-introduction-to-react/basic-example/app.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>First React App!</h1>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));

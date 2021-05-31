07c4e6299d8921f6dc01b22b78719f9cdfb5e23b
eminero <minero.emer@gmail.com>
2021-05-31 16:26:12 -0600

    adding my first custom component

--- 01-introduction-to-react/basic-example/app.jsx
+++ 01-introduction-to-react/basic-example/app.jsx
@@ -1,8 +1,19 @@
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Here is my component</h2>
+                <p>Awesome!</p>
+            </div>
+        );
+    }
+}
 class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>First React App!</h1>
+                <h1>Second React App!</h1>
+                <MyComponent />
             </div>
         );
     }

9879f31dcc037afa8850042ec67b3d7a957d78e4
eminero <minero.emer@gmail.com>
2021-06-01 20:55:08 -0600

    adding conditional JSX

--- 01-introduction-to-react/basic-example/app.jsx
+++ 01-introduction-to-react/basic-example/app.jsx
@@ -11,14 +11,17 @@ class MyComponent extends React.Component {
 class App extends React.Component {
     render() {
         var style = { color: 'red' };
+        var level = this.props.level;
+
         return (
             <div>
                 <h1 style={style}>Hello, you are a {this.props.role}!</h1>
                 <MyComponent />
                 <p className="primary-text">We are using a className property here</p>
+                <p> As a {level} {level === 'beginner' ? (<h2>You are learning the basics</h2>) : (<h2>You know more than the basics</h2>)}</p>
             </div>
         );
     }
 }
 
-ReactDOM.render(<App role="developer" />, document.getElementById("app"));
+ReactDOM.render(<App role="developer" level="beginner"/>, document.getElementById("app"));
